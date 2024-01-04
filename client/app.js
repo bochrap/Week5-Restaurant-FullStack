e9546ebee78c12204c1b1c998a4302d3f0b3d876
AdrianaFeifer <feifer.adriana@yahoo.com>
2024-01-04 14:16:57 +0000

    shopping basket

--- client/app.js
+++ client/app.js
@@ -37,3 +37,51 @@ async function getMenu() {
 }
 
 getMenu();
+
+const productPrices = {
+  fishQuantity: 12.99,
+  pieQuantity: 10.99,
+  bangersQuantity: 9.99,
+  platterQuantity: 8.99,
+  puddingQuantity: 5.99,
+  teaQuantity: 2.99,
+  pimmsQuantity: 7.99,
+  lemonadeQuantity: 3.99
+
+};
+
+// Function to calculate the total
+function calculateTotal() {
+  let total = 0;
+  const quantities = document.querySelectorAll('.quantity');
+
+  quantities.forEach(quantity => {
+    const productId = parseInt(quantity.id.replace('quantity', ''));
+    const price = productPrices[productId];
+    const quantityValue = parseInt(quantity.value);
+    total += price * quantityValue;
+  });
+
+  return total;
+}
+
+// Func to update num of items & total
+function updateCart() {
+  const total = calculateTotal();
+  document.getElementById('total').textContent = total;
+}
+
+// Func to place order
+function placeOrder() {
+  const total = calculateTotal();
+  alert(`Your total is £${total}. Order placed! Your driver is on the way!`);
+}
+
+// Event list 4 quant change
+const quantities = document.querySelectorAll('.quantity');
+quantities.forEach(quantity => {
+  quantity.addEventListener('change', updateCart);
+});
+
+// Initial updt of the basket
+updateCart();
