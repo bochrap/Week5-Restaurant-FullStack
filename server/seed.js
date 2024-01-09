3562a9d5e6fbb2190894267b840c48c2d5e23e4f
bochrap <131978926+bochrap@users.noreply.github.com>
2024-01-09 15:20:23 +0000

    change imgs paths

--- server/seed.js
+++ server/seed.js
@@ -15,15 +15,15 @@ db.exec(`
 db.exec(`
     INSERT INTO menu (name, description, price, category, image)
     VALUES
-    ('Fish and Chips', 'Classic deep-fried fish fillets served with thick-cut fries', '12.99', 'mains','/client/menu-imgs/fishandchips.jpg'),
-    ('Shepherds Pie', 'Ground lamb cooked with vegetables, topped with mashed potatoes', '10.99', 'mains','/client/menu-imgs/shepherdspie_2077_16x9.jpg'),
-    ('Bangers and Mash', 'Sausages served with creamy mashed potatoes and onion gravy',  '9.99', 'mains','/client/menu-imgs/bangersandmash.jpg'),
-    ('Ploughmans Platter', 'Assorted cheeses, pickles, and bread with butter',  '8.99', 'sides','/client/menu-imgs/ploughmansplatter.jpg'),
-    ('Bubble and Squeak', 'Pan-fried leftovers, typically potatoes and cabbage',  '6.99', 'sides','/client/menu-imgs/bubbleandsqueak.jpg'),
-    ('Yorkshire Pudding', 'Baked batter pudding, often served with roast beef', '5.99', 'sides','/client/menu-imgs/yorkiepudding.jpg'),
-    ('English Breakfast Tea', 'Traditional black tea served with milk and sugar', '2.99', 'drinks','/client/menu-imgs/englishtea.jpg'),
-    ('Pimms Cup', 'Refreshing cocktail with Pimms No. 1, fruit, and soda', '7.99', 'drinks','/client/menu-imgs/pimmscup.jpg'),
-    ('Lemonade', 'Homemade lemonade with fresh lemons and sugar', '3.99', 'drinks','/client/menu-imgs/lemonade.jpg')
+    ('Fish and Chips', 'Classic deep-fried fish fillets served with thick-cut fries', '12.99', 'mains','./menu-imgs/fishandchips.jpg'),
+    ('Shepherds Pie', 'Ground lamb cooked with vegetables, topped with mashed potatoes', '10.99', 'mains','./menu-imgs/shepherdspie_2077_16x9.jpg'),
+    ('Bangers and Mash', 'Sausages served with creamy mashed potatoes and onion gravy',  '9.99', 'mains','./menu-imgs/bangersandmash.jpg'),
+    ('Ploughmans Platter', 'Assorted cheeses, pickles, and bread with butter',  '8.99', 'sides','./menu-imgs/ploughmansplatter.jpg'),
+    ('Bubble and Squeak', 'Pan-fried leftovers, typically potatoes and cabbage',  '6.99', 'sides','./menu-imgs/bubbleandsqueak.jpg'),
+    ('Yorkshire Pudding', 'Baked batter pudding, often served with roast beef', '5.99', 'sides','./menu-imgs/yorkiepudding.jpg'),
+    ('English Breakfast Tea', 'Traditional black tea served with milk and sugar', '2.99', 'drinks','./menu-imgs/englishtea.jpg'),
+    ('Pimms Cup', 'Refreshing cocktail with Pimms No. 1, fruit, and soda', '7.99', 'drinks','./menu-imgs/pimmscup.jpg'),
+    ('Lemonade', 'Homemade lemonade with fresh lemons and sugar', '3.99', 'drinks','./menu-imgs/lemonade.jpg')
 `);
 
 const createOrderTable = db.prepare(`
